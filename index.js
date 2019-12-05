e88d33c8dce9f4cb936b54cd00a354a21af9d553
tringuyen623 <tringuyen2391@gmail.com>
2019-12-05 23:04:11 +0700

    update

--- index.js
+++ index.js
@@ -10,21 +10,16 @@ document.getElementById('startReset').onclick = function () {
     } else {
         playing = true;
         score = 0;
-
         document.getElementById('scoreValue').innerHTML = score;
-
         show('timeRemaining');
         timeRemaining = 5;
         document.getElementById('timeRemainingValue').innerHTML = timeRemaining;
         hide('gameOver');
-        hide('operators')
-        document.getElementById('startReset').innerHTML = 'Reset Game';
-
+        hide('operators');
+        hide('inCorrect');
+        document.getElementById('startReset').innerHTML = 'Chơi lại';
         startCountDown();
-
         generateQA();
-
-
     }
 }
 
@@ -32,7 +27,6 @@ let clickAnswer = document.getElementById('choices');
 clickAnswer.addEventListener('click', (e) => {
     const { target } = e;
     let a = target;
-    console.log(a.innerHTML)
     if (playing === true) {
         if (a.innerHTML == correctAnswer) {
             score++;
@@ -44,12 +38,11 @@ clickAnswer.addEventListener('click', (e) => {
             }, 1000)
             generateQA();
             timeRemaining = 5;
-
         } else {
             stopCountDown();
             playing = false;
             show('gameOver');
-            document.getElementById('gameOver').innerHTML = `<p>Game over!</p><p>Your score is ${score}!`;
+            document.getElementById('gameOver').innerHTML = `<p>Kết thúc!</p><p>Điểm của bạn là: ${score}!`;
             hide('timeRemaining');
             hide('correct');
             show('inCorrect');
@@ -65,11 +58,11 @@ const startCountDown = () => {
             stopCountDown();
             playing = false;
             show('gameOver');
-            document.getElementById('gameOver').innerHTML = `<p>Game over!</p><p>Your score is ${score}!`;
+            document.getElementById('gameOver').innerHTML = `<p>Kết thúc!</p><p>Điểm của bạn là: ${score}!`;
             hide('timeRemaining');
             hide('correct');
             hide('inCorrect');
-            document.getElementById('startReset').innerHTML = 'Start Game';
+            // document.getElementById('startReset').innerHTML = 'Chơi lại';
         }
     }, 1000)
 }
@@ -90,9 +83,6 @@ const generateQA = () => {
     let x = Math.floor(Math.random() * 10 + 1);
     let y = Math.floor(Math.random() * 10 + 1);
     let ops = document.getElementById('operators').value;
-    console.log("TCL: generateQA -> ops", ops)
-    // let opsIndex = Math.floor(Math.random() * ops.length);
-
     switch (ops) {
         case 'plus': document.getElementById('question').innerHTML = `${x} + ${y}`;
             correctAnswer = x + y;
@@ -115,7 +105,7 @@ const generateQA = () => {
             do {
                 x = Math.floor(Math.random() * 10 + 10);
                 y = Math.floor(Math.random() * 10 + 1);
-            } while (x%y !==0 || x < y)
+            } while (x % y !== 0 || x < y)
             document.getElementById('question').innerHTML = `${x} : ${y}`;
             correctAnswer = x / y;
             break;
@@ -131,7 +121,32 @@ const generateQA = () => {
 
             let incorrectAnswer;
             do {
-                incorrectAnswer = (Math.floor(Math.random() * 10 + 1)) * (Math.floor(Math.random() * 10 + 1));
+                switch (ops) {
+                    case 'plus':
+                        incorrectAnswer = (Math.floor(Math.random() * 10 + 1)) + (Math.floor(Math.random() * 10 + 1));
+                        break;
+
+                    case 'minus':
+                        do {
+                            x = Math.floor(Math.random() * 10 + 1);
+                            y = Math.floor(Math.random() * 10 + 1);
+                        } while (x < y)
+                        incorrectAnswer = (Math.floor(Math.random() * 10 + 1)) - (Math.floor(Math.random() * 10 + 1));
+                        break;
+
+                    case 'multiply': 
+                    incorrectAnswer = (Math.floor(Math.random() * 10 + 1)) * (Math.floor(Math.random() * 10 + 1));
+                        break;
+
+                    case 'divide':
+                        do {
+                            x = Math.floor(Math.random() * 10 + 10);
+                            y = Math.floor(Math.random() * 10 + 1);
+                        } while (x % y !== 0 || x < y)
+                        incorrectAnswer = (Math.floor(Math.random() * 10 + 1)) / (Math.floor(Math.random() * 10 + 1));
+                        break;
+                }
+
             } while (answers.indexOf(incorrectAnswer) > -1);
             document.getElementById(`box${i}`).innerHTML = incorrectAnswer;
             answers.push(incorrectAnswer);
